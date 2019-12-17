a2375e886e5c1c791874efcd6d02e959feb86f2c
Flavio Ever <flavio.ever@icloud.com>
2019-12-17 01:52:17 -0300

    Yup - Create() Validation

--- src/app/controllers/SignUpController.js
+++ src/app/controllers/SignUpController.js
@@ -1,9 +1,31 @@
 import {} from 'date-fns';
+import * as Yup from 'yup';
+
 import Usuario from '../schemas/Usuario';
 
 class SignUpController {
   async create(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      senha: Yup.string()
+        .required()
+        .min(6),
+      telefones: Yup.array().of(
+        Yup.object().shape({
+          numero: Yup.string().required(),
+          ddd: Yup.string().required(),
+        })
+      ),
+    });
+
     const user = req.body;
+
+    if (!(await schema.isValid(user))) {
+      return res.json({ error: 'Falha na validação do formulário' });
+    }
 
     const usuario = await Usuario.create([
       {
@@ -14,7 +36,7 @@ class SignUpController {
       },
     ]);
 
-    res.json(usuario);
+    return res.json(usuario);
   }
 }
 
